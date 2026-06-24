f61fdb4044a25057a50b6f999dc49a663d9c6275
nipovaa <abeldsonndiku018@gmail.com>
2026-06-24 18:50:56 +0300

    Update and rename .GitHub/Workflows to apps/web/app/page.tsx

---
+++ apps/web/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Nipovaa SaaS Platform</h1>
+
+      <p>Admin | Reseller | Customer 
+    System</p>
+    </div>
+    );
+}
